676b23e9063e1bf778f1d0b5cf3678a26693ff65
IulianToma <iuliantm94@gmail.com>
2015-05-26 21:21:28 +0200

    ChatAppMDW

---
+++ Chat_Client_APP/Chat_Client_APP/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Chat_Client_APP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textLocalPort = new System.Windows.Forms.TextBox();
+            this.textLocalIp = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textFriendsPort = new System.Windows.Forms.TextBox();
+            this.textFriendsIp = new System.Windows.Forms.TextBox();
+            this.textMessage = new System.Windows.Forms.TextBox();
+            this.listMessage = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textLocalPort);
+            this.groupBox1.Controls.Add(this.textLocalIp);
+            this.groupBox1.Location = new System.Drawing.Point(30, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(376, 213);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "PORT";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textLocalPort
+            // 
+            this.textLocalPort.Location = new System.Drawing.Point(130, 101);
+            this.textLocalPort.Name = "textLocalPort";
+            this.textLocalPort.Size = new System.Drawing.Size(165, 22);
+            this.textLocalPort.TabIndex = 1;
+            // 
+            // textLocalIp
+            // 
+            this.textLocalIp.Location = new System.Drawing.Point(130, 63);
+            this.textLocalIp.Name = "textLocalIp";
+            this.textLocalIp.Size = new System.Drawing.Size(165, 22);
+            this.textLocalIp.TabIndex = 0;
+            this.textLocalIp.TextChanged += new System.EventHandler(this.textLocalIp_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textFriendsPort);
+            this.groupBox2.Controls.Add(this.textFriendsIp);
+            this.groupBox2.Location = new System.Drawing.Point(550, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(359, 213);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "PORT";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "IP";
+            // 
+            // textFriendsPort
+            // 
+            this.textFriendsPort.Location = new System.Drawing.Point(105, 101);
+            this.textFriendsPort.Name = "textFriendsPort";
+            this.textFriendsPort.Size = new System.Drawing.Size(152, 22);
+            this.textFriendsPort.TabIndex = 1;
+            // 
+            // textFriendsIp
+            // 
+            this.textFriendsIp.Location = new System.Drawing.Point(105, 63);
+            this.textFriendsIp.Name = "textFriendsIp";
+            this.textFriendsIp.Size = new System.Drawing.Size(152, 22);
+            this.textFriendsIp.TabIndex = 0;
+            // 
+            // textMessage
+            // 
+            this.textMessage.Location = new System.Drawing.Point(30, 422);
+            this.textMessage.Name = "textMessage";
+            this.textMessage.Size = new System.Drawing.Size(777, 22);
+            this.textMessage.TabIndex = 2;
+            // 
+            // listMessage
+            // 
+            this.listMessage.FormattingEnabled = true;
+            this.listMessage.ItemHeight = 16;
+            this.listMessage.Location = new System.Drawing.Point(30, 253);
+            this.listMessage.Name = "listMessage";
+            this.listMessage.Size = new System.Drawing.Size(879, 132);
+            this.listMessage.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(929, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(834, 422);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Send";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1021, 475);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listMessage);
+            this.Controls.Add(this.textMessage);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textLocalPort;
+        private System.Windows.Forms.TextBox textLocalIp;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textFriendsPort;
+        private System.Windows.Forms.TextBox textFriendsIp;
+        private System.Windows.Forms.TextBox textMessage;
+        private System.Windows.Forms.ListBox listMessage;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
