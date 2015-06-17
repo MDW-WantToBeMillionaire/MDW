db6d50a6cb619eeea10001f63a44535f50707b02
yidiwu <dd123fr@hotmail.com>
2015-06-18 00:26:56 +0200

    modify

--- Millionaire/Client/MDWquizz/Login.Designer.cs
+++ Millionaire/Client/MDWquizz/Login.Designer.cs
@@ -32,29 +32,31 @@
             this.tb_id = new System.Windows.Forms.TextBox();
             this.tb_pwd = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.bt_login = new System.Windows.Forms.Button();
             this.bt_register = new System.Windows.Forms.Button();
+            this.bt_login = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(73, 66);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(91, 90);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.Size = new System.Drawing.Size(68, 12);
             this.label1.TabIndex = 0;
             this.label1.Text = "PalyerID:";
             // 
             // tb_id
             // 
-            this.tb_id.Location = new System.Drawing.Point(152, 63);
+            this.tb_id.Location = new System.Drawing.Point(169, 81);
             this.tb_id.Name = "tb_id";
             this.tb_id.Size = new System.Drawing.Size(100, 21);
             this.tb_id.TabIndex = 1;
             // 
             // tb_pwd
             // 
-            this.tb_pwd.Location = new System.Drawing.Point(152, 87);
+            this.tb_pwd.Location = new System.Drawing.Point(169, 125);
             this.tb_pwd.Name = "tb_pwd";
             this.tb_pwd.Size = new System.Drawing.Size(100, 21);
             this.tb_pwd.TabIndex = 3;
@@ -63,37 +65,40 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(73, 90);
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(91, 134);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.Size = new System.Drawing.Size(68, 12);
             this.label2.TabIndex = 2;
             this.label2.Text = "Password:";
             // 
-            // bt_login
-            // 
-            this.bt_login.Location = new System.Drawing.Point(84, 128);
-            this.bt_login.Name = "bt_login";
-            this.bt_login.Size = new System.Drawing.Size(75, 21);
-            this.bt_login.TabIndex = 4;
-            this.bt_login.Text = "Login";
-            this.bt_login.UseVisualStyleBackColor = true;
-            this.bt_login.Click += new System.EventHandler(this.bt_login_Click);
-            // 
             // bt_register
             // 
-            this.bt_register.Location = new System.Drawing.Point(165, 128);
+            this.bt_register.Image = global::MDWquizz.Properties.Resources._1434595098_users_groups;
+            this.bt_register.Location = new System.Drawing.Point(205, 176);
             this.bt_register.Name = "bt_register";
-            this.bt_register.Size = new System.Drawing.Size(74, 21);
+            this.bt_register.Size = new System.Drawing.Size(35, 35);
             this.bt_register.TabIndex = 5;
-            this.bt_register.Text = "Register";
             this.bt_register.UseVisualStyleBackColor = true;
             this.bt_register.Click += new System.EventHandler(this.bt_register_Click);
+            // 
+            // bt_login
+            // 
+            this.bt_login.Image = global::MDWquizz.Properties.Resources._1434595084_lock;
+            this.bt_login.Location = new System.Drawing.Point(114, 176);
+            this.bt_login.Name = "bt_login";
+            this.bt_login.Size = new System.Drawing.Size(35, 35);
+            this.bt_login.TabIndex = 4;
+            this.bt_login.UseVisualStyleBackColor = true;
+            this.bt_login.Click += new System.EventHandler(this.bt_login_Click);
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(326, 205);
+            this.BackgroundImage = global::MDWquizz.Properties.Resources.light_blue_background_2;
+            this.ClientSize = new System.Drawing.Size(428, 302);
             this.Controls.Add(this.bt_register);
             this.Controls.Add(this.bt_login);
             this.Controls.Add(this.tb_pwd);
--- Millionaire/Client/MDWquizz/Register.Designer.cs
+++ Millionaire/Client/MDWquizz/Register.Designer.cs
@@ -28,79 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.bt_confirm = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.bt_confirm = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // bt_confirm
-            // 
-            this.bt_confirm.Location = new System.Drawing.Point(94, 215);
-            this.bt_confirm.Name = "bt_confirm";
-            this.bt_confirm.Size = new System.Drawing.Size(75, 25);
-            this.bt_confirm.TabIndex = 5;
-            this.bt_confirm.Text = "Comfirm";
-            this.bt_confirm.UseVisualStyleBackColor = true;
-            this.bt_confirm.Click += new System.EventHandler(this.bt_confirm_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(39, 36);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(101, 58);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.Size = new System.Drawing.Size(32, 12);
             this.label1.TabIndex = 6;
-            this.label1.Text = "ID";
+            this.label1.Text = "ID：";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(39, 98);
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(101, 110);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(46, 12);
             this.label2.TabIndex = 7;
-            this.label2.Text = "Name";
+            this.label2.Text = "Name：";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(39, 162);
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(101, 159);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.Size = new System.Drawing.Size(74, 12);
             this.label3.TabIndex = 8;
-            this.label3.Text = "Password";
+            this.label3.Text = "Password：";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(124, 33);
+            this.textBox1.Location = new System.Drawing.Point(172, 49);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
             this.textBox1.TabIndex = 9;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(124, 95);
+            this.textBox2.Location = new System.Drawing.Point(172, 101);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
             this.textBox2.TabIndex = 10;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(124, 159);
+            this.textBox3.Location = new System.Drawing.Point(172, 150);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
             this.textBox3.TabIndex = 11;
             this.textBox3.UseSystemPasswordChar = true;
             // 
+            // bt_confirm
+            // 
+            this.bt_confirm.Image = global::MDWquizz.Properties.Resources._1434597861_calendar;
+            this.bt_confirm.Location = new System.Drawing.Point(172, 199);
+            this.bt_confirm.Name = "bt_confirm";
+            this.bt_confirm.Size = new System.Drawing.Size(35, 35);
+            this.bt_confirm.TabIndex = 5;
+            this.bt_confirm.UseVisualStyleBackColor = true;
+            this.bt_confirm.Click += new System.EventHandler(this.bt_confirm_Click);
+            // 
             // Register
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(263, 284);
+            this.BackgroundImage = global::MDWquizz.Properties.Resources.light_blue_background_2;
+            this.ClientSize = new System.Drawing.Size(428, 302);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
